4a3ae12a04d42ad95c264c3186e18b09dbef9268
koogawa <koogawa.app@gmail.com>
2016-05-14 10:44:19 +0900

    Add Podfile

---
+++ Pods/Target Support Files/Pods-RxSwiftSample/Pods-RxSwiftSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftSample : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftSample
+@end
---
+++ Pods/Target Support Files/Pods-RxSwiftSample/Pods-RxSwiftSample-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RxSwiftSampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RxSwiftSampleVersionString[];
+
