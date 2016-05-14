d216099d7133f539dfb2643e77ca8a5164adbde8
koogawa <koogawa.app@gmail.com>
2016-05-14 16:54:49 +0900

    Add rx_text sample

--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+let disposeBag = DisposeBag()
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.textField.rx_text
+            .map {"「\($0)」"}
+        .bindTo(label.rx_text)
+        .addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
