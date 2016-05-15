b64ed4f3551588214eebe6320e64ff708b6388ed
koogawa <koogawa.app@gmail.com>
2016-05-15 12:49:31 +0900

    Non-Reactive Version

--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -7,32 +7,69 @@
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
+import SwiftyJSON
+import FoursquareAPIClient
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    
-    var viewModel = ViewModel()
-    var dataSource = DataSource()
-    var delegate = Delegate()
-    let disposeBag = DisposeBag()
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.delegate = self.delegate
-        self.viewModel.fetch()
-        self.viewModel.venues
-            .asDriver()
-            .drive (
-                self.tableView.rx_itemsWithDataSource(self.dataSource)
-            )
-            .addDisposableTo(self.disposeBag)
+        self.fetch()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Private
+
+    func fetch() {
+            let client = FoursquareAPIClient(accessToken: "YOUR_TOKEN")
+            let parameter: [String: String] = [
+                "ll": "40.7,-74",
+            ];
+            client.requestWithPath("venues/search", parameter: parameter) {
+                [weak self] (data, error) in
+                let json = JSON(data: data!)
+                let venues = (self?.parseVenues(json["response"]["venues"])) ?? [Venue]()
+                observer.on(.Next(venues))
+                observer.on(.Completed)
+        }
+    }
+
+    func parseVenues(venuesJSON: JSON) -> [Venue] {
+        var venues = [Venue]()
+        for (key: _, venueJSON: JSON) in venuesJSON {
+            venues.append(Venue(json: JSON))
+        }
+        return venues
+    }
+    
+    // MARK: - UITableView DataSource
+
+    public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.venues.count
+    }
+
+    public func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("VenueCell", forIndexPath: indexPath)
+        cell.textLabel?.text = self.venues[indexPath.row].name
+        cell.detailTextLabel?.text = self.venues[indexPath.row].address
+        return cell
+    }
+
+    // MARK: - UITableView Delegate
+
+    public func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
     }
 }
